3af8bcb558efbfe450e34e2739041b5c3e9a0bd1
Martin Musale <martinmshale@gmail.com>
2019-12-06 10:39:12 +0300

    Add solution for day 6 part 2 in python

--- day_6/py3/main.py
+++ day_6/py3/main.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 map_values = [v.strip().split(")") for v in open("day_6/input.txt").readlines()]
 
 values = {}
@@ -8,7 +10,21 @@ for value in map_values:
     if b not in values:
         values[b] = []
     values[a].append(b)
-    # values[b].append(a)
+    values[b].append(a)
+
+distance = {}
+q = deque()
+q.append(("YOU", 0))
+
+while q:
+    a, b = q.popleft()
+    if a in distance:
+        continue
+    distance[a] = b
+    for i in values[a]:
+        q.append((i, b + 1))
+
+print(distance["SAN"] - 2)
 
 
 def loop(k):
@@ -19,8 +35,8 @@ def loop(k):
     return s
 
 
-total = 0
-for k in values:
-    total += loop(k)
+# total = 0
+# for k in values:
+#     total += loop(k)
 
-print(total)
+# print(total)
