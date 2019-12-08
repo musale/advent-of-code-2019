ffa99424466f34b7647d7e7873ba6882c491cc6c
Martin Musale <martinmshale@gmail.com>
2019-12-08 21:56:36 +0300

    Added the sanitized solution for day 8 part 2 in py3

--- day_08/py3/main.py
+++ day_08/py3/main.py
@@ -21,6 +21,28 @@ def part_one(images):
     return ones * twos
 
 
+def part_two(images, width, height):
+    decoded_image = ""
+    size = width * height
+    layers = len(images) // (width * height)
+
+    for i in range(size):
+        for j in range(layers):
+            pixel = images[i + (j * size)]
+            if pixel != 2:
+                decoded_image += str(pixel)
+                break
+    return "".join(str(i) for i in decoded_image)
+
+
+def print_image(image, width, height):
+    start = 0
+    for i in range(height):
+        line = image[start : width + start]
+        start += width
+        print("".join(["." if j == "0" else "#" for j in line]))
+
+
 if __name__ == "__main__":
     image_data = [int(i) for i in open("day_08/input.txt").read().strip()]
     width = 25
@@ -32,4 +54,8 @@ if __name__ == "__main__":
         layer_data = image_data[current_pos : (width * height) + current_pos]
         current_pos += width * height
         image_layers[i] = layer_data
+
     print(part_one(image_layers))
+
+    decoded_image = part_two(image_data, width, height)
+    print_image(decoded_image, width, height)
