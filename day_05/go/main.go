fb81d4e0d8182d40df6627cd5e0e229efce4e0c5
Martin Musale <martinmshale@gmail.com>
2019-12-09 14:40:19 +0300

    Re-write day 5 part 1 go solution

--- day_05/go/main.go
+++ day_05/go/main.go
@@ -8,152 +8,125 @@ import (
 	"strings"
 )
 
-func main() {
-	stateValues, err := ioutil.ReadFile("day_05/input.txt")
-	if err != nil {
-		log.Fatalf("failed to open file with error %v", err)
+// code - single integer in an IntCode program
+type code int
+
+// IntCode - program for a machine that can run int code
+type IntCode struct {
+	Input    []code
+	Outputs  []code
+	Modes    []code
+	Instructions []code
+	StartPos code
+	RelBase  code
+	Running  bool
+	Paused   bool
+}
+
+// Tick - starts an int code program with the provided input
+func (c *IntCode) Tick() {
+	// Calculate the modes
+	mode := c.Input[c.StartPos]
+	opcode := mode % 100
+	c.Modes = []code{(mode / 100) % 10, (mode / 1000) % 10, (mode / 10000) % 10}
+	switch opcode {
+	case 1:
+		c.Input[c.getAddr(3)] = c.getArgument(1) + c.getArgument(2)
+		c.StartPos += 4
+	case 2:
+		c.Input[c.getAddr(3)] = c.getArgument(1) * c.getArgument(2)
+		c.StartPos += 4
+	case 3:
+		if len(c.Instructions) > 0{
+			c.Input[c.getAddr(1)] = c.Instructions[0]
+			c.Instructions = c.Instructions[1:]
+			c.StartPos += 2
+		} else{
+			c.Paused = true
+		}
+	case 4:
+		c.Outputs = append(c.Outputs, c.getArgument(1))
+		c.StartPos +=2
+	case 99:
+		c.Running = false
 	}
-	stateStrings := strings.Split(string(stateValues), ",")
-	states := make([]int, len(stateStrings))
+}
+
+// Run - an IntCode machine
+func (c *IntCode) Run() {
+	for c.Running != false {
+		c.Tick()
+	}
+}
+
+// getArgument - of a given position
+func (c *IntCode) getArgument(pos code) code {
+	arg := c.Modes[pos-1]
+	var val code
+	switch arg {
+	case 0:
+		val = c.Input[c.Input[c.StartPos+pos]]
+	case 1:
+		val = c.Input[c.StartPos+pos]
+	case 2:
+		val = c.Input[c.RelBase+c.Input[c.StartPos+pos]]
+	default:
+		log.Fatalf("Unknown argument mode %d", arg)
+	}
+	return val
+}
+
+// getAddr - at a given position
+func (c *IntCode) getAddr(pos code) code {
+	arg := c.Modes[pos-1]
+	var val code
+	switch arg {
+	case 0:
+		val = c.Input[c.StartPos+pos]
+	case 1:
+		log.Fatalf("Failed getting arg at %d with immediate argument mode", arg)
+	case 2:
+		val = c.RelBase + c.Input[c.StartPos+pos]
+	default:
+		log.Fatalf("Unknown argument mode %d", arg)
+	}
+	return val
+}
+
+// readInput - from file and convert to code
+func readInput() []code {
+	inputValues, err := ioutil.ReadFile("day_05/input.txt")
+	if err != nil {
+		log.Fatalf("Failed to open file with error %v", err)
+	}
+	stateStrings := strings.Split(string(inputValues), ",")
+	input := make([]code, len(stateStrings))
 
 	for j, s := range stateStrings {
-		states[j], _ = strconv.Atoi(s)
+		num, _ := strconv.Atoi(s)
+		input[j] = code(num)
 	}
+	return input
+}
 
-	pos := 0
-	for states[pos] != 99 {
-		instructions := strings.Split(strconv.Itoa(states[pos]), "")
-		modes := []int{}
-
-		for _, m := range instructions {
-			mode, _ := strconv.Atoi(m)
-			modes = append(modes, mode)
-		}
-
-		opscode := modes[len(modes)-1]
-		if len(modes) == 1 {
-			modes = []int{0}
-		} else {
-			modes = modes[:len(modes)-2]
-		}
-		for len(modes) < 3 {
-			modes = append([]int{0}, modes...)
-		}
-		switch opscode {
-		case 1:
-			ins1, ins2, ins3 := states[pos+1], states[pos+2], states[pos+3]
-			var first, second int
-			if modes[2] == 1 {
-				first = ins1
-			} else {
-				first = states[ins1]
-			}
-			if modes[1] == 1 {
-				second = ins2
-			} else {
-				second = states[ins2]
-			}
-			states[ins3] = first + second
-			pos += 4
-		case 2:
-			ins1, ins2, ins3 := states[pos+1], states[pos+2], states[pos+3]
-			var first, second int
-
-			if modes[2] == 1 {
-				first = ins1
-			} else {
-				first = states[ins1]
-			}
-			if modes[1] == 1 {
-				second = ins2
-			} else {
-				second = states[ins2]
-			}
-			states[ins3] = first * second
-			pos += 4
-		case 3:
-			ins1 := states[pos+1]
-			states[ins1] = 5 // Temporary input
-			pos += 2
-		case 4:
-			ins1 := states[pos+1]
-			fmt.Println(states[ins1])
-			pos += 2
-		case 5:
-			ins1, ins2 := states[pos+1], states[pos+2]
-			var first int
-			if modes[1] == 1 {
-				first = ins1
-			} else {
-				first = states[ins1]
-			}
-			if first != 0 {
-				if modes[0] == 1 {
-					pos = ins2
-				} else {
-					pos = states[ins2]
-				}
-			} else {
-				pos += 3
-			}
-		case 6:
-			ins1, ins2 := states[pos+1], states[pos+2]
-			var first int
-			if modes[1] == 1 {
-				first = ins1
-			} else {
-				first = states[ins1]
-			}
-			if first == 0 {
-				if modes[0] == 1 {
-					pos = ins2
-				} else {
-					pos = states[ins2]
-				}
-			} else {
-				pos += 3
-			}
-		case 7:
-			ins1, ins2, ins3 := states[pos+1], states[pos+2], states[pos+3]
-			var first, second int
-			if modes[2] == 1 {
-				first = ins1
-			} else {
-				first = states[ins1]
-			}
-			if modes[1] == 1 {
-				second = ins2
-			} else {
-				second = states[ins2]
-			}
-			if first < second {
-				states[ins3] = 1
-			} else {
-				states[ins3] = 0
-			}
-			pos += 4
-		case 8:
-			ins1, ins2, ins3 := states[pos+1], states[pos+2], states[pos+3]
-			var first, second int
-			if modes[2] == 1 {
-				first = ins1
-			} else {
-				first = states[ins1]
-			}
-			if modes[1] == 1 {
-				second = ins2
-			} else {
-				second = states[ins2]
-			}
-			if first == second {
-				states[ins3] = 1
-			} else {
-				states[ins3] = 0
-			}
-			pos += 4
-		default:
-			num := states[pos]
-			log.Fatalf("unknown opscode %d from state value %v", opscode, num)
+// maxOutput - gets the largest value in the output array
+func (c *IntCode) maxOutput() code {
+	var max code
+	for i := 0; i < len(c.Outputs); i++ {
+		curr := c.Outputs[i]
+		if curr > max{
+			max = curr
 		}
 	}
+	return max
+}
+
+func main() {
+	partOneInput := readInput()
+	var intCode IntCode
+	intCode.Running = true
+	intCode.Input = partOneInput
+	intCode.Instructions = []code{code(1)}
+	intCode.Run()
+	fmt.Println("Part 1:", intCode.maxOutput())
 }
