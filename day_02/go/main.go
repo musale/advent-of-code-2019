1ffe30a89578bf427486e00c7c3b7df134c7ad7a
Martin Musale <martinmshale@gmail.com>
2019-12-09 13:32:31 +0300

    Re-write Day 2 IntCode as base for IntCode puzzles

--- day_02/go/main.go
+++ day_02/go/main.go
@@ -8,42 +8,95 @@ import (
 	"strings"
 )
 
-func main() {
-	for noun := 0; noun < 99; noun++ {
-		for verb := 0; verb < 99; verb++ {
-			stateValues, err := ioutil.ReadFile("day_02/input.txt")
-			if err != nil {
-				log.Fatalf("failed to open file with error %v", err)
-			}
-			stateStrings := strings.Split(string(stateValues), ",")
-			states := make([]int, len(stateStrings))
+type code int
 
-			for j, s := range stateStrings {
-				states[j], _ = strconv.Atoi(s)
-			}
-			i := 0
-			states[1] = noun // noun
-			states[2] = verb // verb
-			for states[i] != 99 {
-				switch states[i] {
-				case 1:
-					// add
-					sum := states[states[i+1]] + states[states[i+2]]
-					states[states[i+3]] = sum
-					i += 4
-				case 2:
-					// multiply
-					mul := states[states[i+1]] * states[states[i+2]]
-					states[states[i+3]] = mul
-					i += 4
-				}
-			}
-			if states[0] == 19690720 {
-				fmt.Println(noun)
-				fmt.Println(verb)
-				fmt.Println(100*noun + verb)
-				return
-			}
-		}
+// IntCode - machine that can run int code
+type IntCode struct {
+	Input    []code
+	Outputs  []code
+	Modes    [3]code
+	StartPos code
+	RelBase  code
+	Running  bool
+	Paused   bool
+}
+
+// Tick - starts an int code program with the provided input
+func (c *IntCode) Tick() {
+	// Calculate the modes
+	mode := c.Input[c.StartPos]
+	opcode := mode % 100
+	c.Modes = [3]code{(mode / 100) % 10, (mode / 1000) % 10, (mode / 10000) % 10}
+	c.Paused = false
+
+	switch opcode {
+	case 1:
+		c.Input[c.getAddr(3)] = c.getArgument(1) + c.getArgument(2)
+		c.StartPos += 4
+	case 2:
+		c.Input[c.getAddr(3)] = c.getArgument(1) * c.getArgument(2)
+		c.StartPos += 4
+	case 99:
+		c.Running = false
 	}
+}
+
+// Run - an IntCode machine
+func (c *IntCode) Run() {
+	for c.Running != false {
+		c.Tick()
+	}
+}
+
+// getArgument - of a given position
+func (c *IntCode) getArgument(pos code) code {
+	arg := c.Modes[pos-1]
+	var val code
+	switch arg {
+	case 0:
+		val = c.Input[c.Input[c.StartPos+pos]]
+	case 1:
+		val = c.Input[c.StartPos+pos]
+	case 2:
+		val = c.Input[c.RelBase+c.Input[c.StartPos+pos]]
+	default:
+		log.Fatalf("Unknown argument mode %d", arg)
+	}
+	return val
+}
+
+// getAddr - at a given position
+func (c *IntCode) getAddr(pos code) code {
+	arg := c.Modes[pos-1]
+	var val code
+	switch arg {
+	case 0:
+		val = c.Input[c.StartPos+pos]
+	case 1:
+		log.Fatalf("Failed getting arg at %d with immediate argument mode", arg)
+	case 2:
+		val = c.RelBase + c.Input[c.StartPos+pos]
+	default:
+		log.Fatalf("Unknown argument mode %d", arg)
+	}
+	return val
+}
+
+func main() {
+	inputValues, err := ioutil.ReadFile("day_02/input.txt")
+	if err != nil {
+		log.Fatalf("Failed to open file with error %v", err)
+	}
+	stateStrings := strings.Split(string(inputValues), ",")
+	input := make([]code, len(stateStrings))
+
+	for j, s := range stateStrings {
+		num, _ := strconv.Atoi(s)
+		input[j] = code(num)
+	}
+	var intCode IntCode
+	intCode.Running = true
+	intCode.Input = input
+	intCode.Run()
+	fmt.Println("Part 1: ", intCode.Input[0])
 }
